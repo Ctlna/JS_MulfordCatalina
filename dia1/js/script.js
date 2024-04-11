723b129efba2f813d15c87e169d27134121c7691
Catalina <catalinamulfordmonroy@gmail.com>
2024-04-11 08:37:34 -0500

    feat: :alembic: Practica
    Ejemplos de lo que son bucles, funciones y booleanos

--- dia1/js/script.js
+++ dia1/js/script.js
@@ -1,5 +1,68 @@
+// Practica y Ejercicios Dìa 1
+//Catalina Mulford Monroy
+
+
 let a = 1;
 let b = 16;
+let te = "Texto"
+var quiero = "pasear"
 
+console.log(quiero)
 console.log (a+b)
 console.log(typeof 9);
+console.log(typeof te);
+
+var quiero = "ya no";
+console.log(quiero)
+
+
+// Boleanos
+
+let S = true
+let N = false
+
+console.log ("La variable S es: " ,S)
+console.log ("La variable N es: " ,N)
+console.log("AND: && ", S&&S)
+console.log("OR: || ", S||N)
+console.log("NOT: ! ", !N)
+
+
+//Funciòn sin retorno sin parametros
+function funcionNormal(){
+    console.log("La funciòn");
+}
+console.log(funcionNormal());
+
+
+//Funciòn sin retorno con parametros
+function funcionNorma(a,b){
+    console.log(a*b);
+}
+funcionNorma(a,b);
+
+//Funciòn con retorno con parametros
+function funcionNor(c,d){
+    return c+d;
+}
+console.log(funcionNor(5,8));
+
+//Funciòn con retorno sin parametros
+function funcionNormalita(a,b){
+    return "Devolvio";
+}
+console.log(funcionNormalita());
+
+
+// Bucles
+
+arreglo=[123,"dsf",true]
+console.log(arreglo.length);
+
+tamano=arreglo.length;
+
+for (let i=0;i<tamano;i++){
+    console.log(arreglo[i])
+}
+
+// -------------- Ejercicios --------------------------
