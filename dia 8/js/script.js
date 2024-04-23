e698a356b9c039127b66bb3786e61146f7eb9f0a
Catalina <catalinamulfordmonroy@gmail.com>
2024-04-23 07:53:49 -0500

    feat: :construction: Se implementa mas codigo
    Se continua implementando mas codigo

--- dia 8/js/script.js
+++ dia 8/js/script.js
@@ -6,8 +6,22 @@ function fetchSwapi(){
         if (this.readyState===4 && this.status===200){
             let response = JSON.parse(this.responseText);
             console.log(response);
-            eldisplay(response);
+            funcionar(response);
+        }
+        else if(this.readyState===4){
+            console.log('Error: ',this.statusText);
+
         }
     };
     xhr.send();
+}
+function funcionar(dataL){
+    let info=document.getElementById('espacio')
+    if (dataL.response==="error"){
+        info.innerHTML=`<p>Error:${dataL.error}</p>`;
+    }
+    else{
+        info.innerHTML=`
+        <p></p>
+        `}
 }
