a07f5b217c1b89523fef02ca36bb9626e548a807
Catalina <catalinamulfordmonroy@gmail.com>
2024-04-22 19:40:25 -0500

    feat: :sparkles: Más información del array
    Se creo el codigo para mostrar más información del array

--- dia 7/js/script.js
+++ dia 7/js/script.js
@@ -23,7 +23,43 @@ function displayHero(data){
     }
     else{
         hInfo.innerHTML=`
+        <p></p>
         <p>Name:${data.name}</p>
-        <p>Inteligencia:${data.powerstats.intelligence}`
+        <p>Inteligencia:${data.powerstats}
+        <p>Inteligencia:${data.powerstats.intelligence}
+        <p>Inteligencia:${data.powerstats.strength}
+        <p>Inteligencia:${data.powerstats.speed}
+        <p>Inteligencia:${data.powerstats.durability}
+        <p>Inteligencia:${data.powerstats.power}
+        <p>Inteligencia:${data.powerstats.combat}
+        <p></p>
+        <p>Inteligencia:${data.biography}
+        <p>Inteligencia:${data.biography.full-name}
+        <p>Inteligencia:${data.biography.alter-egos}
+        <p>Inteligencia:${data.biography.aliases}
+        <p>Inteligencia:${data.biography.place-of-birth}
+        <p>Inteligencia:${data.biography.first-appearance}
+        <p>Inteligencia:${data.biography.publisher}
+        <p>Inteligencia:${data.biography.alignment}
+        <p></p>
+        <p>Inteligencia:${data.appearance}
+        <p>Inteligencia:${data.appearance.gender}
+        <p>Inteligencia:${data.appearance.race}
+        <p>Inteligencia:${data.appearance.height}
+        <p>Inteligencia:${data.appearance.weight}
+        <p>Inteligencia:${data.appearance.eye-color}
+        <p>Inteligencia:${data.appearance.hair-color}
+        <p></p>
+        <p>Inteligencia:${data.work}
+        <p>Inteligencia:${data.work.occupation}
+        <p>Inteligencia:${data.work.base}
+        <p></p>
+        <p>Inteligencia:${data.connections}
+        <p>Inteligencia:${data.connections.group-affiliation}
+        <p>Inteligencia:${data.connections.relatives}
+        <p></p>
+        <p>Inteligencia:${data.image}
+        <p>Inteligencia:${data.work.url}
+        `
     }
 }
