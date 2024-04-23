1c7f4db292afd0f4d4f5c984ce4bb07ed9eb7574
Catalina <catalinamulfordmonroy@gmail.com>
2024-04-23 09:51:46 -0500

    feat: :sparkles: Se creo la raiz día 9
    Se creo la raiz día 9 para guardar los nuevos cambios del día 8

---
+++ dia 9/js/script.js
@@ -0,0 +1,65 @@
+function fetchSwapi(){
+    xhr = new XMLHttpRequest();
+    let url = `https://swapi.py4e.com/api/people/1/`
+    xhr.open('GET',url,true);
+    xhr.onreadystatechange=function(){
+        if (this.readyState===4 && this.status===200){
+            let response = JSON.parse(this.responseText);
+            console.log(response);
+            funcionar(response);
+        }
+        else if(this.readyState===4){
+            console.log('Error: ',this.statusText);
+
+        }
+    };
+    xhr.send();
+}
+function funcionar(dataL){
+    let info=document.getElementById('espacio')
+    if (dataL.response==="error"){
+        info.innerHTML=`<p>Error:${dataL.error}</p>`;
+    }
+    else{
+        info.innerHTML=`
+        <p></p>
+        <p>Name:${dataL.name}</p>
+        <p>Height:${dataL.height}</p>
+        <p>Mass:${dataL.mass}</p>
+        <p>Hair-color:${dataL.hair_color}</p>
+        <p>Skin-color:${dataL.skin_color}</p>
+        <p>Eye-color:${dataL.eye_color}</p>
+        <p>Birth-year:${dataL.birth_year}</p>
+        <p>Gender:${dataL.gender}</p>
+        <p></p>
+
+        <p>Homeworld:</p>
+        <p><a href= ${dataL.homeworld}>${dataL.homeworld}</a></p>
+
+        <p></p>
+
+        <p>Films:</p>
+        <p><a href ="https://swapi.py4e.com/api/films/1/">Films 1</a></p>
+        <p><a href ="https://swapi.py4e.com/api/films/2/">Films 2</a></p>
+        <p><a href ="https://swapi.py4e.com/api/films/3/">Films 3</a></p>
+        <p><a href ="https://swapi.py4e.com/api/films/6/">Films 6</a></p>
+        <p><a href ="https://swapi.py4e.com/api/films/7/>Films 7</a></p>
+        <p></p>
+
+        <p>Species:</p>
+        <p><a href="${dataL.species}">${dataL.species}</a></p>
+        <p></p>
+
+        <p>Vehicles:</p>
+        <p><a href= ${dataL.vehicles}>${dataL.vehicles}</a></p>
+        <p></p>
+
+        <p>Starships:</p>
+        <p><a href="${dataL.starships}">${dataL.starships}</a></p>
+        
+        <p>Created: ${dataL.created}</p>
+        <p>Edited: ${dataL.edited}</p>
+        <p>url:</p>
+        <p><a href= ${dataL.url}>${dataL.url}</a></p>
+        `}
+}
