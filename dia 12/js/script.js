a2eccc24470e9dd75fffb55bf7a4ad32049d5107
Catalina <catalinamulfordmonroy@gmail.com>
2024-05-01 01:09:18 -0500

    feat: :construction: Codigo para comparar
    Codigo para comparar la respuesta y las cartas

--- dia 12/js/script.js
+++ dia 12/js/script.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 let baraja = "";
+
 function fetchInicio(){
     let xhr = new XMLHttpRequest();
     let url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`;
@@ -24,15 +25,16 @@ function fetchInicio(){
     };
     xhr.send();
 }
+
 function buscar(as){
     let xhro = new XMLHttpRequest();
-    let url = `https://deckofcardsapi.com/api/deck/${as}/draw/?count=1`;
+    let url = `https://deckofcardsapi.com/api/deck/${as}/draw/?count=2`;
     xhro.open('GET',url,true);
     xhro.onreadystatechange=function(){
         if (this.readyState===4 && this.status===200){
             let response = JSON.parse(this.responseText);
             console.log(response);
-            cosito(response);
+            cosito(response.cards);
         }
         else if(this.readyState===4){
             console.log('Error: ',this.statusText);
@@ -40,31 +42,80 @@ function buscar(as){
     }; 
     xhro.send();
 }
+
 function cosito(cartas){
     let carat = document.getElementById('comparar');
     let confirma = document.getElementById('confirma');    
-    if (cartas.response==="error"){
-        carat.innerHTML= `<p>Error:${cartas.response}</p>`;
+    if (cartas[0].value === "error" || cartas[1].value === "error"){
+        carat.innerHTML= `<p>Error: algo está mal</p>`;
     }
     else{
         const comprar=document.getElementById("comparar").value;
         confirma.addEventListener('click',(event) => {
-                console.log("Se puso "+comprar);           // BIEN... se repite pero esta bien
+            console.log("Se puso "+comprar);           // BIEN... se repite pero está bien
 
-                if (comprar==="mayor"){
-                    console.log("es mayor");
+            let resultado = devolver(cartas); // Llamada a la función devolver para determinar el resultado
+
+            if (comprar==="mayor"){
+                if(resultado === "mayor"){
+                    console.log("Ganaste");
+                } else {
+                    console.log("Perdiste");
                 }
-                else if (comprar==="menor"){
-                    console.log("es menor");
+            }
+            else if (comprar==="menor"){
+                if(resultado === "menor"){
+                    console.log("Ganaste");
+                } else {
+                    console.log("Perdiste");
                 }
-                else{
-                    console.log("Lo ingresado no es valido");
+            }
+            else {
+                if(resultado === "igual"){
+                    console.log("Ganaste");
+                } else {
+                    console.log("Perdiste");
                 }
-            });
+            }
+        });
     }
 }
+
 document.getElementById('confirma').addEventListener('click', function() {
     cosito(); 
 });
 
-        //carat.innerHTML= `<carat class="imagen" src="${cartas.cards.images.svg}"/>`;
+function devolver(ruta){
+    let cartaCero="";
+    let cartaUno="";
+    // valor carta 0
+    if (ruta[0].value==="QUEEN" || ruta[0].value==="KING" || ruta[0].value==="JACK"){
+        cartaCero=10;
+    }
+    else if(ruta[0].value==="ACE"){
+        cartaCero=1;
+    }
+    else{
+        cartaCero=parseInt(ruta[0].value);
+    }
+    // valor carta 1
+    if (ruta[1].value==="QUEEN" || ruta[1].value==="KING" || ruta[1].value==="JACK"){
+        cartaUno=10;
+    }
+    else if(ruta[1].value==="ACE"){
+        cartaUno=1;
+    }
+    else{
+        cartaUno=parseInt(ruta[1].value);
+    }
+    //
+    if (cartaCero>cartaUno){
+        return "mayor";
+    }
+    else if(cartaCero<cartaUno){
+        return "menor";
+    }
+    else{
+        return "igual";
+    }
+}
