b52e6845e2ed512892a9bfc3232a6c4909c51161
Catalina <catalinamulfordmonroy@gmail.com>
2024-05-03 08:04:28 -0500

    feat: :construction: Codigo
    Codigo del json

---
+++ dia 13/js/script.js
@@ -0,0 +1,13 @@
+var heroes=[]
+
+function fetchNHeroe(){
+    let personaje = document.getElementById('nPersonaje').value;
+    let actor = document.getElementById('nActor').value
+    let edad = document.getElementById('eActor').value
+    let ubicacion = document.getElementById('ubicacion').value
+    let poster = document.getElementById('poster').value
+    let fecha = document.getElementById('fecha').value
+    let productora = document.getElementById('productora').value
+
+    
+}
