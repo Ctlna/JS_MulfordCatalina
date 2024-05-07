e5e249ffb56f3bbef493341c4e7b8c63d2f90229
Catalina <catalinamulfordmonroy@gmail.com>
2024-05-07 08:32:32 -0500

    feat: :zap: mejora del codigo
    mejora del codigo

--- dia 15/js/script.js
+++ dia 15/js/script.js
@@ -6,6 +6,11 @@ window.onload=function lazar(){
         if (this.readyState===4 && this.status===200){
             let response = JSON.parse(this.responseText);
             console.log(response)
+            info(response);
+            email(response);
+            naci(response);
+            ubi(response);
+            celu(response);
             clave(response);
         }
         else if(this.readyState===4){
@@ -16,96 +21,107 @@ window.onload=function lazar(){
 }
 
 function info(elLink){
-    let que = document.getElementById('claro');
-    let info=document.getElementById('oscuro');
-    if (elLink.response==="error"){
-        info.innerHTML=`<p>Error:${elLink.error}</p>`;
-    }
-    else{
-        que.innerHTML=`Hi, My name is`
-        corto=elLink.results[0]
-        
-        info.innerHTML=`
-        <h1>${corto.name.first}  ${corto.name.last}</h1>
-        `
+    if (document.getElementById('name').onmousemove=true){
+        let que = document.getElementById('claro');
+        let info=document.getElementById('oscuro');
+        if (elLink.response==="error"){
+            info.innerHTML=`<p>Error:${elLink.error}</p>`;
+        }
+        else{
+            que.innerHTML=`Hi, My name is`
+            corto=elLink.results[0]
+            
+            info.innerHTML=`
+            <h1>${corto.name.first}  ${corto.name.last}</h1>
+            `};
     }
 }
 
 function email(elLink){
-    let que = document.getElementById('claro');
-    let info=document.getElementById('oscuro');
-    if (elLink.response==="error"){
-        info.innerHTML=`<p>Error:${elLink.error}</p>`;
-    }
-    else{
-        que.innerHTML=`My email address is`
-        corto=elLink.results[0]
-        info.innerHTML=`
-        <h1>${corto.email}</h1>
-        `
+    if (document.getElementById('email').onmousemove=true){ 
+        let que = document.getElementById('claro');
+        let info=document.getElementById('oscuro');
+        if (elLink.response==="error"){
+            info.innerHTML=`<p>Error:${elLink.error}</p>`;
+        }
+        else{
+            que.innerHTML=`My email address is`
+            corto=elLink.results[0]
+            info.innerHTML=`
+            <h1>${corto.email}</h1>
+            `
+        }
     }
 }
 
 function naci(elLink){
-    let que = document.getElementById('claro');
-    let info=document.getElementById('oscuro');
-    if (elLink.response==="error"){
-        info.innerHTML=`<p>Error:${elLink.error}</p>`;
-    }
-    else{
-        que.innerHTML=`My birthday is`
-        corto=elLink.results[0]
-        
-        info.innerHTML=`
-        <h1>${corto.dob.date}</h1>
-        `
+    if (document.getElementById('birth').onmousemove=true){
+        let que = document.getElementById('claro');
+        let info=document.getElementById('oscuro');
+        if (elLink.response==="error"){
+            info.innerHTML=`<p>Error:${elLink.error}</p>`;
+        }
+        else{
+            que.innerHTML=`My birthday is`
+            corto=elLink.results[0]
+            
+            info.innerHTML=`
+            <h1>${corto.dob.date}</h1>
+            `
+        }
     }
 }
 
 function ubi(elLink){
-    let que = document.getElementById('claro');
-    let info=document.getElementById('oscuro');
-    if (elLink.response==="error"){
-        info.innerHTML=`<p>Error:${elLink.error}</p>`;
-    }
-    else{
-        que.innerHTML=`My address is`
-        corto=elLink.results[0]
-        
-        info.innerHTML=`
-        <h1>${corto.location.street.number}  ${corto.location.street.name} </h1>
-        `
+    if (document.getElementById('ubi').onmousemove=true){
+        let que = document.getElementById('claro');
+        let info=document.getElementById('oscuro');
+        if (elLink.response==="error"){
+            info.innerHTML=`<p>Error:${elLink.error}</p>`;
+        }
+        else{
+            que.innerHTML=`My address is`
+            corto=elLink.results[0]
+            
+            info.innerHTML=`
+            <h1>${corto.location.street.number}  ${corto.location.street.name} </h1>
+            `
+        }
     }
 }
 
 function celu(elLink){
-    let que = document.getElementById('claro');
-    let info=document.getElementById('oscuro');
-    if (elLink.response==="error"){
-        info.innerHTML=`<p>Error:${elLink.error}</p>`;
-    }
-    else{
-        que.innerHTML=`Hi, My phone number is`
-        corto=elLink.results[0]
-        
-        info.innerHTML=`
-        <h1>${corto.phone}</h1>
-        `
+    if (document.getElementById('phone').onmousemove=true){
+        let que = document.getElementById('claro');
+        let info=document.getElementById('oscuro');
+        if (elLink.response==="error"){
+            info.innerHTML=`<p>Error:${elLink.error}</p>`;
+        }
+        else{
+            que.innerHTML=`Hi, My phone number is`
+            corto=elLink.results[0]
+            
+            info.innerHTML=`
+            <h1>${corto.phone}</h1>
+            `
+        }
     }
 }
 
 function clave(elLink){
-    let que = document.getElementById('claro');
-    let info=document.getElementById('oscuro');
-    if (elLink.response==="error"){
-        info.innerHTML=`<p>Error:${elLink.error}</p>`;
-    }
-    else{
-        que.innerHTML=`Hi, My password is`
-        corto=elLink.results[0]
-        
-        info.innerHTML=`
-        <h1>${corto.login.password}</h1>
-        `
+    if (document.getElementById('clave').onmousemove=true){
+        let que = document.getElementById('claro');
+        let info=document.getElementById('oscuro');
+        if (elLink.response==="error"){
+            info.innerHTML=`<p>Error:${elLink.error}</p>`;
+        }
+        else{
+            que.innerHTML=`Hi, My password is`
+            corto=elLink.results[0]
+            
+            info.innerHTML=`
+            <h1>${corto.login.password}</h1>
+            `
+        }
     }
 }
